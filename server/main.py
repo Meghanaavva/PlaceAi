dea17324b40730197013c69b722be9447a72e13d
Meghana <mavva@gitam.in>
2026-05-14 17:40:36 +0530

    fixed code

--- server/main.py
+++ server/main.py
@@ -28,11 +28,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://place-ai-omega.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
